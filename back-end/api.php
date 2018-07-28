cefb2803d914582417152800f099b0241752ed63
David Thorn <david.thorn@atino.de>
2018-07-28 17:16:41 +0200

    completed

--- back-end/api.php
+++ back-end/api.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+
 echo json_encode([
     "message" => "Hello world from the backend application"
 ], JSON_PRETTY_PRINT);
--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,3 +1,5 @@
+const apiURL = "http://localhost:4200/api.php";
+
 const onWindowLoadedEvent = () => {
     const ele = document.getElementById('welcome_text');
     ele.innerHTML = "Welcome to you";
@@ -14,4 +16,25 @@ window.onload = onWindowLoadedEvent
 function startApplication() {
     const ele = document.getElementById('welcome_text');
     ele.innerHTML = "Application started";
+    getWelcomeMessage(ele);
+}
+
+/// Execute a HTTP GET request to http://localhost:4200/api.php
+/// If the response is success then the message from the api should then be shown in 
+/// the window
+/// If the response is not success then an error message should be shown
+const getWelcomeMessage = (ele) => {
+
+    $.get(apiURL , (data, status) => {
+        switch(status) {
+            case "success":
+            const json = JSON.parse(data);
+            ele.innerHTML = json.message;
+            break;
+            default:
+            ele.innerHTML = "An error occurred";
+            break;
+        }
+     })
+
 }
